82625299531f944f8e1a3aafaf298e68e72c99ef
Surek András <andras.surek@facekom.com>
2025-12-23 20:00:00 +0100

    feat: add parser for the request line

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,64 +1,31 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
 	"log"
 	"net"
-	"strings"
 
 	"github.com/absurek/go-http-from-tcp/internal/constants"
+	"github.com/absurek/go-http-from-tcp/internal/request"
 )
-
-func readLinesChannel(f io.ReadCloser) <-chan string {
-	lineCh := make(chan string)
-
-	go func() {
-		defer f.Close()
-		defer close(lineCh)
-
-		var line string
-		buffer := make([]byte, 8)
-		for {
-			n, err := f.Read(buffer)
-			if err != nil {
-				switch {
-				case errors.Is(err, io.EOF):
-					lineCh <- line + string(buffer[:n])
-				default:
-					log.Fatalf("Error: %v", err)
-				}
-
-				return
-			}
-
-			parts := strings.Split(string(buffer[:n]), "\n")
-			for i, part := range parts {
-				if i < len(parts)-1 {
-					lineCh <- line + part
-					line = ""
-				} else {
-					line += part
-				}
-			}
-		}
-	}()
-
-	return lineCh
-}
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	fmt.Println("Connection accepted")
+	log.Println("Connection accepted")
 
-	lineCh := readLinesChannel(conn)
-	for line := range lineCh {
-		fmt.Println(line)
+	req, err := request.RequestFromReader(conn)
+	if err != nil {
+		log.Printf("Error: request from conn: %v", err)
 	}
 
-	fmt.Println("Connection closed")
+	fmt.Println("Request line:")
+	fmt.Println("- Method:", req.RequestLine.Method)
+	fmt.Println("- Target:", req.RequestLine.RequestTarget)
+	fmt.Println("- Version:", req.RequestLine.HttpVersion)
+	fmt.Println()
+
+	log.Println("Connection closed")
 }
 
 func main() {
